68c7654a9dc51a22c0304d243d0f51a898c39863
Boris Bügling <boris@icculus.org>
2013-08-09 21:45:43 +0200

    Initial Commit

---
+++ Me/BBUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BBUAppDelegate.h
+//  Me
+//
+//  Created by Boris Bügling on 09.08.13.
+//  Copyright (c) 2013 Boris Bügling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MeTests/MeTests.m
@@ -0,0 +1,34 @@
+//
+//  MeTests.m
+//  MeTests
+//
+//  Created by Boris Bügling on 09.08.13.
+//  Copyright (c) 2013 Boris Bügling. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MeTests : XCTestCase
+
+@end
+
+@implementation MeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
